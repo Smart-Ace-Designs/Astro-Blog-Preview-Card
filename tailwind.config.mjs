0f32b0b2fbece2411401db42dd1b99b3f04f4875
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2024-06-24 13:50:49 -0500

    Sets initial Tailwind CSS configuration and styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        PrimaryYellow: "hsl(47, 88%, 63%)",
+        NeutralWhite: "hsl(0, 0%, 100%)",
+        NeutralGrey: "hsl(0, 0%, 50%)",
+        NeutralBlack: "hsl(0, 0%, 7%)",
+      },
+      fontFamily: {
+        figtree: ["Figtree", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
