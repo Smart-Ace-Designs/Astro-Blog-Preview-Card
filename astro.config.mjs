0e2c2fecba45c9640741dfaf8c26743d2c2a7a1c
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2026-03-10 11:53:48 -0500

    Remove experimental flag for Fonts API

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,15 +8,13 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  experimental: {
-    fonts: [
-      {
-        provider: fontProviders.google(),
-        name: "Figtree",
-        cssVariable: "--font-figtree",
-        weights: [400, 500, 800],
-        styles: ["normal"],
-      },
-    ],
-  },
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "Figtree",
+      cssVariable: "--font-figtree",
+      weights: [400, 500, 800],
+      styles: ["normal"],
+    },
+  ],
 });
